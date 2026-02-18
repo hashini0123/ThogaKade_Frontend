474f13ec82bcdb990405c9c8a799888592c46b54
hashini0123 <nanayakkarahashini5@gmail.com>
2026-02-18 13:36:02 +0530

    feat: add Home component with routing, layout, and styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,17 @@ import { Customer } from './page/dashboard/customer/customer';
 import { Item } from './page/dashboard/item/item';
 import { Order } from './page/dashboard/order/order';
 import { DashRoot } from './page/dashboard/dash-root/dash-root';
+import { Home } from './page/home/home';
 
 export const routes: Routes = [
+
     {
         path:"",
+        component:Home
+    },
+
+    {
+        path:"login",
         component:Login
     },
 
