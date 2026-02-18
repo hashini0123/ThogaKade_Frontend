0f969b5f978a91aede680e0cc6048afbf455d2cd
hashini0123 <nanayakkarahashini5@gmail.com>
2026-02-18 11:20:29 +0530

    feat: update sidebar links and enhance login form structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
+import { Login } from './page/login/login';
+import { Dashboard } from './page/dashboard/dashboard';
+import { Customer } from './page/dashboard/customer/customer';
+import { Item } from './page/dashboard/item/item';
+import { Order } from './page/dashboard/order/order';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"login",
+        component:Login
+    },
+
+    {
+        path:"dashboard",
+        component:Dashboard,
+        children:[
+            {
+                path:"customer",
+                component:Customer
+            },
+            {
+                path:"item",
+                component:Item
+            },
+            {
+                path:"order",
+                component:Order
+            }
+        ]
+    }
+];
