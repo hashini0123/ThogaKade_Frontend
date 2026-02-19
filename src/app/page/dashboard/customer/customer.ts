b583e1ed31a74530e02b56ba01602628213e157b
hashini0123 <nanayakkarahashini5@gmail.com>
2026-02-19 12:59:21 +0530

    feat: enhance customer component with add customer form and data fetching

--- src/app/page/dashboard/customer/customer.ts
+++ src/app/page/dashboard/customer/customer.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { CustomerModel } from '../../../../model/type';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
@@ -9,19 +9,24 @@ import { CommonModule } from '@angular/common';
   templateUrl: './customer.html',
   styleUrl: './customer.css',
 })
-export class Customer {
+export class Customer implements OnInit{
+    customerList:Array<CustomerModel> = [];
 
-  customerList:Array<CustomerModel> = [];
-
-  constructor(private http:HttpClient) {
-    this.getAll();
+  constructor(private http:HttpClient,private cdr:ChangeDetectorRef) {
 
   } 
+
+  ngOnInit(): void {
+    this.getAll();
+  }
   
   getAll() {
     this.http.get<CustomerModel[]>("http://localhost:8080/get").subscribe(data =>{
       this.customerList = data;
+      this.cdr.detectChanges();   
     })
   }
 
+  
 }
+
