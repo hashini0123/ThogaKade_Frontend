20917cf200998682b421638a98561a7bc31b2bdf
hashini0123 <nanayakkarahashini5@gmail.com>
2026-02-20 12:08:17 +0530

    angular backend #1

--- src/app/page/dashboard/customer/customer.ts
+++ src/app/page/dashboard/customer/customer.ts
@@ -2,15 +2,30 @@ import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { CustomerModel } from '../../../../model/type';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-customer',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './customer.html',
   styleUrl: './customer.css',
 })
 export class Customer implements OnInit{
-    customerList:Array<CustomerModel> = [];
+    customerList: Array<CustomerModel> = [];
+    showAddForm = false;
+    customerObj: CustomerModel = {
+      custID: '',
+      custTitle: '',
+      custName: '',
+      DOB: {},
+      salary: 0.0,
+      custAddress: '',
+      city: '',
+      province: '',
+      postalcode: '',
+    }
+    
 
   constructor(private http:HttpClient,private cdr:ChangeDetectorRef) {
 
@@ -24,6 +39,22 @@ export class Customer implements OnInit{
     this.http.get<CustomerModel[]>("http://localhost:8080/get").subscribe(data =>{
       this.customerList = data;
       this.cdr.detectChanges();   
+    })
+  }
+
+  addCustomer(): void {
+
+    console.log(this.customerObj);
+    this.http.post("http://localhost:8080/add", this.customerObj).subscribe(data => {
+      console.log(data);
+      if(data === true){
+        Swal.fire({
+          title: "Good job!",
+          text: "You clicked the button!",
+          icon: "success"
+        });
+      }
+      this.getAll();
     })
   }
 
--- src/model/type.ts
+++ src/model/type.ts
@@ -1,11 +1,22 @@
-export interface CustomerModel{
-    custID:string;
-    custName:string;
-    custTitle:string;
-    DOB:object;
-    salary:number;
-    custAddress:string;
-    city:string;
-    province:string;
-    postalcode:string;
+export interface CustomerModel {
+    custID: string;
+    custTitle: string;
+    custName: string;
+    DOB: object;
+    salary: number;
+    custAddress: string;
+    city: string;
+    province: string;
+    postalcode: string;
 }
+
+
+// String custID;
+//     private String custName;
+//     private String custTitle;
+//     private LocalDate DOB;
+//     private double salary;
+//     private String custAddress;
+//     private String city;
+//     private String province;
+//     private String postalcode;
